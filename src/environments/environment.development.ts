8f99a7ee041cb0c38d4dc140a6544862d6948266
Deiviidsito <david.alvarez.barraza@gmail.com>
2025-03-12 16:16:52 -0300

    feat: login page y logo de la empresa

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  SUPABASE_URL: 'https://omovfywonzshygdekpwp.supabase.co',
+  SUPABASE_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tb3ZmeXdvbnpzaHlnZGVrcHdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4MDYwODgsImV4cCI6MjA1NzM4MjA4OH0.iCUFIcbgYmWal3TtLK07MUVI3ZccNLMPT_gC9baue8c',
+  SUPABASE_ANON_KEY: '<your_supabase_anon_key>',
+};
